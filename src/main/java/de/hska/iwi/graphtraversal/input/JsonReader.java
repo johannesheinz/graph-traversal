be76e08d68ff2029eb220498583b07f94cf0b57b
Johannes Heinz <johannesmartinheinz@gmail.com>
2017-11-15 17:41:39 +0100

    Impleneted JSON configuration reader and parser via Jackson library

--- src/main/java/de/hska/iwi/graphtraversal/input/JsonReader.java
+++ src/main/java/de/hska/iwi/graphtraversal/input/JsonReader.java
@@ -1,7 +1,42 @@
 package de.hska.iwi.graphtraversal.input;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 public class JsonReader {
 
-    public JsonReader(String filename) {
+    private final ObjectMapper mapper;
+
+    /**
+     * Initializes the reader.
+     */
+    public JsonReader() {
+        this.mapper = new ObjectMapper();
+    }
+
+    /**
+     * Reads a given JSON document from the resources folder.
+     *
+     * @param filename The file name of the JSON file in the resources folder
+     * @return A parsed object that contains all the information from the JSON document or null if an error occurred
+     * @throws InvalidGraphConfigurationException if an error occurs during parsing of the JSON document
+     */
+    public GraphConfiguration readConfig(String filename) throws InvalidGraphConfigurationException {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(filename).getFile());
+
+        GraphConfiguration config = null;
+
+        try {
+            config = mapper.readValue(file, GraphConfiguration.class);
+
+        } catch (IOException e) {
+            throw new InvalidGraphConfigurationException(e.getMessage());
+        }
+
+        return config;
     }
 }
