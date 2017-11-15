c7e13daae05de9d79cf337cdef298eeb58724d3b
Johannes Heinz <johannesmartinheinz@gmail.com>
2017-11-15 17:42:41 +0100

    Implemented JSON configuration reader and parser via Jackson library

--- src/main/java/de/hska/iwi/graphtraversal/input/GraphConfiguration.java
+++ src/main/java/de/hska/iwi/graphtraversal/input/GraphConfiguration.java
@@ -1,4 +1,73 @@
 package de.hska.iwi.graphtraversal.input;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public class GraphConfiguration {
+
+    @JsonProperty
+    private boolean directed;
+
+    @JsonProperty
+    private List<String> edges;
+
+    @JsonProperty
+    private List<String> nodes;
+
+    @JsonProperty
+    private List<Output> output;
+
+    @JsonProperty
+    private String startNode;
+
+    @JsonProperty
+    private Strategy strategy;
+
+
+    public enum Strategy {
+        @JsonProperty("dfs") DFS,
+        @JsonProperty("bfs") BFS
+    }
+
+    public enum Output {
+        @JsonProperty("table") TABLE,
+        @JsonProperty("gif") GIF
+    }
+
+
+    public boolean isDirected() {
+        return directed;
+    }
+
+    public List<String> getEdges() {
+        return edges;
+    }
+
+    public List<String> getNodes() {
+        return nodes;
+    }
+
+    public List<Output> getOutput() {
+        return output;
+    }
+
+    public String getStartNode() {
+        return startNode;
+    }
+
+    public Strategy getStrategy() {
+        return strategy;
+    }
+
+
+    @Override
+    public String toString() {
+        return "directed? " + directed +
+                ", edges=" + edges +
+                ", nodes=" + nodes +
+                ", output=" + output +
+                ", startNode='" + startNode + '\'' +
+                ", strategy=" + strategy;
+    }
 }
